302e1a3546ba8f5847c88952a0628fa83383a8e1
jason890317 <890317jason@gmail.com>
2024-04-09 17:16:57 +0800

    special

---
+++ llvm_build/lib/Transforms/CFGuard/CMakeFiles/LLVMCFGuard.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jason/Documents/advanced_compiler/llvm-project-17.0.2.src/llvm/lib/Transforms/CFGuard/CFGuard.cpp" "lib/Transforms/CFGuard/CMakeFiles/LLVMCFGuard.dir/CFGuard.cpp.o" "gcc" "lib/Transforms/CFGuard/CMakeFiles/LLVMCFGuard.dir/CFGuard.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jason/Documents/advanced_compiler/llvm_build/lib/IR/CMakeFiles/LLVMCore.dir/DependInfo.cmake"
+  "/home/jason/Documents/advanced_compiler/llvm_build/lib/Support/CMakeFiles/LLVMSupport.dir/DependInfo.cmake"
+  "/home/jason/Documents/advanced_compiler/llvm_build/lib/TargetParser/CMakeFiles/LLVMTargetParser.dir/DependInfo.cmake"
+  "/home/jason/Documents/advanced_compiler/llvm_build/lib/BinaryFormat/CMakeFiles/LLVMBinaryFormat.dir/DependInfo.cmake"
+  "/home/jason/Documents/advanced_compiler/llvm_build/lib/Remarks/CMakeFiles/LLVMRemarks.dir/DependInfo.cmake"
+  "/home/jason/Documents/advanced_compiler/llvm_build/lib/Bitstream/Reader/CMakeFiles/LLVMBitstreamReader.dir/DependInfo.cmake"
+  "/home/jason/Documents/advanced_compiler/llvm_build/lib/Demangle/CMakeFiles/LLVMDemangle.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ llvm_build/lib/Transforms/Scalar/CMakeFiles/LLVMScalarOpts.dir/Scalar.cpp.o.d
@@ -0,0 +1,4 @@
+lib/Transforms/Scalar/CMakeFiles/LLVMScalarOpts.dir/Scalar.cpp.o: \
+ /home/jason/Documents/advanced_compiler/llvm-project-17.0.2.src/llvm/lib/Transforms/Scalar/Scalar.cpp \
+ /usr/include/stdc-predef.h \
+ /home/jason/Documents/advanced_compiler/llvm-project-17.0.2.src/llvm/include/llvm/InitializePasses.h
